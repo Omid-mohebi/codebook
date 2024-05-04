4dea5333c556bd61134226527c2404a343767822
Omid-mohebi <omohebi12@gmail.com>
2024-05-04 13:16:35 +0330

    add: create database models and basic routing

--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+groups = [
+    {'id':1, 'name': 'python'},
+    {'id':2, 'name': 'c#'},
+    {'id':3, 'name': 'C++'},
+]
+
 def home (request):
-    return render(request, 'main/home.html')
+    return render(request, 'main/home.html', {'groups':groups})
+
+def group (request, pk):
+    return render(request, 'main/group.html', {'group': groups[int(pk)-1]})
